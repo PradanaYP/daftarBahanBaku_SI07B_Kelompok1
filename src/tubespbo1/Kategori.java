aff8bdd04616157ebf0897a10fc101dd9bc9990e
armandasuryan <armanda.surya97@gmail.com>
2024-12-20 20:20:05 +0700

    first commit

---
+++ src/tubespbo1/Kategori.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tubespbo1;
+
+/**
+ *
+ * @author ARMANDA SN
+ */
+public class Kategori {
+    
+}
